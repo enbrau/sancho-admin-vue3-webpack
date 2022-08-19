21d44514a07ccf3c5b0e3187d2eba3d3e89d2f91
enbrau <admin@enbrau.com>
2022-08-19 16:28:02 +0800

    feat add formatter function: toFileSize

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -2,3 +2,15 @@ export function toAmount(amount) {
   const str = amount.toFixed(2) + ''
   return str.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
+
+export function toFileSize(fileByte) {
+  let fileSizeByte = fileByte
+  let fileSizeMsg = ''
+  if (fileSizeByte < 1048576) fileSizeMsg = (fileSizeByte / 1024).toFixed(2) + 'KB'
+  else if (fileSizeByte === 1048576) fileSizeMsg = '1MB'
+  else if (fileSizeByte > 1048576 && fileSizeByte < 1073741824) fileSizeMsg = (fileSizeByte / (1024 * 1024)).toFixed(2) + 'MB'
+  else if (fileSizeByte > 1048576 && fileSizeByte === 1073741824) fileSizeMsg = '1GB'
+  else if (fileSizeByte > 1073741824 && fileSizeByte < 1099511627776) fileSizeMsg = (fileSizeByte / (1024 * 1024 * 1024)).toFixed(2) + 'GB'
+  else fileSizeMsg = '>1TB'
+  return fileSizeMsg
+}
