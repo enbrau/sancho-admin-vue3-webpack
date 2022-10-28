8349bfad98e1e50a37870606b74f075cf1cc129d
Enbrau <enbrau@gmail.com>
2022-10-28 09:12:39 +0800

    chore add new function

--- src/utils/index.js
+++ src/utils/index.js
@@ -136,3 +136,18 @@ export function uuid() {
   });
   return uuid.replaceAll('-', '');
 }
+
+export const dynamicLoadScript = src => {
+  return new Promise((resolve, reject) => {
+    const existingScript = document.getElementById(src)
+    if (existingScript) resolve()
+    if (!existingScript) {
+      const script = document.createElement("script")
+      script.src = src
+      script.id = src
+      document.body.appendChild(script)
+      script.onload = resolve
+      script.onerror = reject
+    }
+  })
+}
